2cdfce1366b767673da622738144c3636d1645e5
ABDUL REHMAN <mr.abdulrehman.ar@gmail.com>
2022-04-27 14:31:59 +0500

    javascript

---
+++ javascript practise/js/script.js
@@ -0,0 +1,42 @@
+console.log("Hello world");
+// used for single line comment
+/* used for multiline comments */
+
+//variables in javascript
+
+var a = 15;
+console.log(a);
+
+var b = -15;
+
+var sum = a + b;
+console.log(sum);
+
+// decimal numbers
+var c = 125.8979; 
+console.log(a + c);
+
+// string in javascript
+var x = "Hello, this is a string in javascript";
+console.log(x);
+
+// boolean in javascript
+let check = false;
+console.log(check);
+check = true;
+console.log(check);
+
+// objects in javascript
+
+let student = {
+   "Name": 'Abdul Rehman',
+   "Roll No": "45-555",
+   "Class": 'CIT'
+};
+
+console.log(student);
+
+// arrays in javascript
+//indexing    0         1           2
+let cars = ["Honda", "Suzuki", "Ferreri"];
+console.log(cars[2]);
